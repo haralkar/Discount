81750c9cc2a5dd4126b514de175f31a76ffe8fdf
Haraldur Karlsson <haralkar@gmail.com>
2014-12-10 21:04:33 +0100

    Rebate

---
+++ Discount/Rebate.cpp
@@ -0,0 +1,75 @@
+#include "stdafx.h"
+#include "Rebate.h"
+
+
+Rebate::Rebate()
+{
+}
+
+
+Rebate::~Rebate()
+{
+}
+
+IData* __stdcall Rebate::Create() 
+{ 
+	return new Rebate(); 
+}
+CreateDataFn Rebate::Creator() const
+{
+	return Create; 
+}
+
+std::string Rebate::DataTitle() const 
+{ 
+	return "rebate"; 
+}
+
+void Rebate::Free() 
+{ 
+	delete this; 
+}
+
+std::string Rebate::Id() 
+{ 
+	return id_; 
+}
+void Rebate::SetId(std::string id) 
+{ 
+	id_ = id; 
+}
+
+std::string Rebate::Name() 
+{ 
+	return name_; 
+}
+void Rebate::SetName(std::string name) 
+{ 
+	name_ = name; 
+}
+
+int Rebate::Procent() 
+{
+	return proCent_;
+}
+void Rebate::SetProcent(int p)
+{
+	proCent_ = p;
+}
+
+std::string Rebate::Client()
+{
+	return client_;
+}
+void Rebate::SetClient(std::string client)
+{
+	client_ = client;
+}
+std::string Rebate::Product()
+{
+	return product_;
+}
+void Rebate::SetProduct(std::string product)
+{
+	product_ = product;
+}
---
+++ Discount/Rebate.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "IData.h"
+class Rebate : public IData
+{
+public:
+	Rebate();
+	virtual ~Rebate();
+	void Free();
+
+	static IData* __stdcall Create();
+	virtual CreateDataFn Creator() const;
+	virtual std::string DataTitle() const final;
+
+
+	virtual std::string Id();
+	virtual void SetId(std::string id);
+
+	virtual std::string Name();
+	virtual void SetName(std::string name);
+
+	virtual int Procent();
+	virtual void SetProcent(int p);
+
+	virtual std::string Client();
+	virtual void SetClient(std::string );
+
+	virtual std::string Product();
+	virtual void SetProduct(std::string);
+
+
+private:
+	std::string id_;
+	std::string name_;
+	std::string client_;
+	std::string product_;
+	int proCent_;
+};
+
