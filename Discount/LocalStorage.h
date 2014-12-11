075a033a9f094c2515e6f6ac9f6ac67693b29c0f
Haraldur Karlsson <haralkar@gmail.com>
2014-12-11 10:09:42 +0100

    storage manages more that single data point

--- Discount/LocalStorage.h
+++ Discount/LocalStorage.h
@@ -14,47 +14,51 @@ class LocalStorage : public IDataStorage<T>
 public:
 	LocalStorage()
 	{
-		T t;
-		DataFactory::Get()->Register(t.DataTitle(), t.Creator());
+		if (!initialised_)
+		{
+			T t;
+			dataTitle_ = t.DataTitle();
+			DataFactory::Get()->Register(dataTitle_, t.Creator());
+			initialised_ = true;
+		}
 	}
-	void Cleanup(){ data_.clear();}
-
-	//virtual ~LocalStorage();
-
-	virtual T& Get();
+	void Cleanup(){ 
+		data_.clear(); 
+		initialised_ = false; 
+	}
 
 	virtual T& Get(std::string name);
 
 	virtual T& Get(std::string name, std::string id);
 protected:
 	static map < std::string, T* > data_;
+	static std::string dataTitle_;
+	static bool initialised_;
 
 };
 template<class T>
 map < std::string, T*> LocalStorage<T>::data_;
+template<class T>
+bool LocalStorage<T>::initialised_ = false;
+template<class T>
+std::string LocalStorage<T>::dataTitle_;
 
 template <class T>
-inline T& LocalStorage<T>::Get()
-{
-	T t;
-	return Get(t.DataTitle());
-}
-template <class T>
-inline T& LocalStorage<T>::Get(std::string name)
+inline T& LocalStorage<T>::Get(std::string id)
 {
 	DataFactory* df = DataFactory::Get();
-	T* d = static_cast<T*>(df->CreateDataInstance(name));
-	data_[name] = d;
+	T* d = static_cast<T*>(df->CreateDataInstance(dataTitle_));
+	d->SetId(id);
+	data_[id] = d;
 	return *d;
 }
 template <class T>
-inline T& LocalStorage<T>::Get(std::string name, std::string id)
+inline T& LocalStorage<T>::Get(std::string , std::string id)
 {
-	auto it = data_.find(name);
+	auto it = data_.find(id);
 	if (it != data_.end())
 		return *it->second;
 
-	T& d = Get(name);
-	d.SetId(id);
+	T& d = Get(id);
 	return d;
 }
--- DiscountTest/storage.cpp
+++ DiscountTest/storage.cpp
@@ -37,12 +37,31 @@ namespace DiscountTest
 			std::string main = "main";
 			Client& client = storage.Get(storage.Client(), main);
 			Assert::AreEqual(main, client.Id());
-			
+
 			std::string name("Name");
 			client.SetName(name);
 
 			Client& client2 = storage.Get(storage.Client(), main);
 			Assert::AreEqual(name, client2.Name());
+		}
+		TEST_METHOD(LocalStorageGetShouldKeepMoreData)
+		{
+			std::string main = "main";
+			std::string other = "other";
+
+			Client& client1 = storage.Get(storage.Client(), main);
+			Client& client2 = storage.Get(storage.Client(), other);
+			
+			Assert::AreEqual(main, client1.Id());
+			Assert::AreEqual(other, client2.Id());
+
+			std::string name("Name");
+			std::string otherName("Other");
+			client1.SetName(name);
+			client2.SetName(other);
+
+			Client& clientAgain = storage.Get(storage.Client(), main);
+			Assert::AreEqual(name, clientAgain.Name());
 		}
 		TEST_METHOD(PersistencyInAllThings)
 		{
@@ -50,8 +69,7 @@ namespace DiscountTest
 			{
 				LocalStorage<Rebate> storage;
 
-				Rebate& rebate = storage.Get();
-				rebate.SetId("tenFlat");
+				Rebate& rebate = storage.Get("rebate", "tenFlat");
 				rebate.SetName("Ten percent");
 				rebate.SetProcent(10);
 			}
